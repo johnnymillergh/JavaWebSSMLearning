9f21be40605ca86b8bfd5d591d93328c657324eb
Johnny Miller <johnnysviva@outlook.com>
2018-06-05 21:56:41 +0800

    Chapter 08: MyBatis Dynamic SQL 2

--- src/chapter08/mybatis_introduction/test/MybatisTest.java
+++ src/chapter08/mybatis_introduction/test/MybatisTest.java
@@ -184,7 +184,7 @@ public class MybatisTest {
         ids.add(3);
 
         int[] array = new int[2];
-        array[0]=1;
+        array[0] = 1;
         array[1] = 2;
 
         List<User> users = sqlSession.selectList("chapter08.mybatis_introduction.mapper.CustomerMapper." +
